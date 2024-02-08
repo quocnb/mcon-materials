6e77c08d7a17b79a1863f4f5f87b2ea7ad1de551
Quoc Nguyen <quocnb.vnu@gmail.com>
2024-02-08 14:29:25 +0900

    Chapter 4: Custom asynchronous sequences with asyncstream

--- 04-custom-asynchronous-sequences-with-asyncstream/projects/starter/Blabber/Model/BlabberModel.swift
+++ 04-custom-asynchronous-sequences-with-asyncstream/projects/starter/Blabber/Model/BlabberModel.swift
@@ -54,6 +54,31 @@ class BlabberModel: ObservableObject {
   /// Does a countdown and sends the message.
   func countdown(to message: String) async throws {
     guard !message.isEmpty else { return }
+		var countdown = 3
+		let counter = AsyncStream<String> {
+			guard countdown >= 0 else {
+				return nil
+			}
+			do {
+				try await Task.sleep(for: .seconds(1))
+			} catch {
+				return nil
+			}
+			defer {
+				countdown -= 1
+			}
+			if countdown == 0 {
+				return "🎉" + message
+			} else {
+				return "\(countdown)"
+			}
+		}
+		try await counter.forEach {
+			try await say($0)
+		}
+//		for await countdownMessage in counter {
+//			try await say(countdownMessage)
+//		}
   }
 
   /// Start live chat updates
@@ -84,6 +109,33 @@ class BlabberModel: ObservableObject {
 
   /// Reads the server chat stream and updates the data model.
   private func readMessages(stream: URLSession.AsyncBytes) async throws {
+		var iterator = stream.lines.makeAsyncIterator()
+		
+		guard let first = try await iterator.next() else {
+			throw "No response from server"
+		}
+		
+		guard let data = first.data(using: .utf8),
+		let status = try? JSONDecoder().decode(ServerStatus.self, from: data)
+		else {
+			throw "Invalid response from server"
+		}
+		messages.append(Message(message: "\(status.activeUsers) active users"))
+		
+		let notifications = Task {
+			await observeAppStatus()
+		}
+		
+		defer {
+			notifications.cancel()
+		}
+		
+		for try await line in stream.lines {
+			if let data = line.data(using: .utf8),
+				 let update = try? JSONDecoder().decode(Message.self, from: data) {
+				messages.append(update)
+			}
+		}
   }
 
   /// Sends the user's message to the chat server
@@ -111,4 +163,26 @@ class BlabberModel: ObservableObject {
     configuration.timeoutIntervalForRequest = .infinity
     return URLSession(configuration: configuration)
   }()
+	
+	func observeAppStatus() async {
+		Task {
+			for await _ in NotificationCenter.default.notifications(for: UIApplication.willResignActiveNotification) {
+				try? await say("\(username) went away", isSystemMessage: true)
+			}
+		}
+		Task {
+			for await _ in NotificationCenter.default.notifications(for: UIApplication.didBecomeActiveNotification) {
+				try? await say("\(username) come back", isSystemMessage: true)
+			}
+		}
+		
+	}
+}
+
+extension AsyncSequence {
+	func forEach(_ body: (Element) async throws -> Void) async throws {
+		for try await element in self {
+			try await body(element)
+		}
+	}
 }
