fb07989513117cfb3d1750b53246ccd3fb184ace
Quoc Nguyen <quocnb.vnu@gmail.com>
2024-02-11 16:40:42 +0900

    Chapter 7: Concurrent code with taskgroup

--- 07-concurrent-code-with-taskgroup/projects/starter/Sky/ScanModel.swift
+++ 07-concurrent-code-with-taskgroup/projects/starter/Sky/ScanModel.swift
@@ -60,7 +60,51 @@ class ScanModel: ObservableObject {
 
   func runAllTasks() async throws {
     started = Date()
+		try await withThrowingTaskGroup(of: Result<String, Error>.self) { [unowned self] group in
+			let batchSize = min(4, total)
+			for number in 0..<batchSize {
+				group.addTask {
+					await self.worker(number: number)
+				}
+			}
+			var index = batchSize
+			for try await result in group {
+				switch result {
+				case .success(let result):
+					print("Completed: \(result)")
+				case .failure(let error):
+					print("Failed: \(error.localizedDescription)")
+				}
+				if index < total {
+					group.addTask { [index] in
+						await self.worker(number: index)
+					}
+					index += 1
+				}
+			}
+
+			await MainActor.run {
+				completed = 0
+				countPerSecond = 0
+				scheduled = 0
+			}
+		}
   }
+
+	func worker(number: Int) async -> Result<String, Error> {
+		await onScheduled()
+
+		let task = ScanTask(input: number)
+		let result: Result<String, Error>
+		do {
+			result = try .success(await task.run())
+		} catch {
+			result = .failure(error)
+		}
+
+		await onTaskCompleted()
+		return result
+	}
 }
 
 // MARK: - Tracking task progress.
